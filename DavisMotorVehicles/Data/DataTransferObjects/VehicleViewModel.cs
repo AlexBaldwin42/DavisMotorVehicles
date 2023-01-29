0d2b449b4d5c17e99fd014190058f9c2bd0303d7
AlexBaldwin42 <blackanode@gmail.com>
2023-01-28 20:34:19 -0700

    Able to add vehicles now

--- DavisMotorVehicles/Data/DataTransferObjects/VehicleViewModel.cs
+++ DavisMotorVehicles/Data/DataTransferObjects/VehicleViewModel.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace DavisMotorVehicles.Data.DataTransferObjects
 {
-	public class VehicleDto
+	public class VehicleViewModel
 	{
 		//public Vehicle Vehicle { get; set; }
 		//public List<Tire>? Tires { get; set; }
 		//public List<TireStatus>? TireStatuses { get; set; }
 		//public VehicleType VehicleTypes { get; set; }
 		public int VehicleId { get; set; }
-		public string VinNumber { get; set; }
+		public string Vin { get; set; }
 		public string Make { get; set; }
 		public string Model { get; set; }
 		public int Year { get; set; }
@@ -21,13 +21,13 @@ namespace DavisMotorVehicles.Data.DataTransferObjects
 		public int FuelLevel { get; set; }
 		public List<TireVm>? Tires { get; set; }
 		public int VehicleTypeId { get; set; }
-		public string VehicleTypeName { get; set; }
+		public string? VehicleTypeName { get; set; }
 
 	}
 	public class TireVm
 	{
 		public int TireId { get; set; }
 		public int TireStatusId { get; set; }
-		public string TireStatus { get; set; }
+		public string? TireStatus { get; set; }
 	}
 }
--- DavisMotorVehicles/Server/Controllers/VehicleController.cs
+++ DavisMotorVehicles/Server/Controllers/VehicleController.cs
@@ -12,7 +12,7 @@ namespace DavisMotorVehicles.Server.Controllers
 	{
 
 		[HttpGet]
-		public IEnumerable<VehicleDto> Get()
+		public IEnumerable<VehicleViewModel> Get()
 		{
 			using var db = new VehicleDatabaseContext();
 
@@ -23,58 +23,96 @@ namespace DavisMotorVehicles.Server.Controllers
 					.Include(i => i.VehicleType)
 					.Include(i => i.Tires)
 					.ThenInclude(i => i.TireStatus).ToList();
-			List<VehicleDto> vehicleDtos = new List<VehicleDto>();
+			List<VehicleViewModel> vehicleDtos = new List<VehicleViewModel>();
 			foreach (var vehicle in vehicles)
 			{
-				VehicleDto vehicleDto = new VehicleDto();
-				vehicleDto.VehicleId = vehicle.Id;
-				vehicleDto.VinNumber = vehicle.VinNumber;
-				vehicleDto.Make = vehicle.Make;
-				vehicleDto.Model = vehicle.Model;
-				vehicleDto.Year = vehicle.Year;
-				vehicleDto.FuelLevel = vehicle.FuelLevel;
-				vehicleDto.VehicleTypeName = vehicle.VehicleType.Name;
-				vehicleDto.VehicleTypeName = vehicle.VehicleType.Name;
-				vehicleDto.Tires = vehicle.Tires.Select(i => new TireVm()
-				{
-					TireId = i.Id,
-					TireStatusId = i.TireStatus.Id,
-					TireStatus = i.TireStatus.Status
-				}).ToList();
-				vehicleDtos.Add(vehicleDto);
+				vehicleDtos.Add(ConvertToVehicleViewModel(vehicle));
 			}
-
 			return vehicleDtos;
-
 		}
 
 		[HttpPost]
-		public void Post(VehicleDto vehicle)
+		public VehicleViewModel Post(VehicleViewModel vehicleViewModel)
 		{
+			Vehicle vehicle = new()
+			{
+				Id = vehicleViewModel.VehicleId,
+				FuelLevel = vehicleViewModel.FuelLevel,
+				Make = vehicleViewModel.Make,
+				Model = vehicleViewModel.Model,
+				Year = vehicleViewModel.Year,
+				VehicleTypeId = vehicleViewModel.VehicleTypeId,
+				VinNumber = vehicleViewModel.Vin
+			};
+
+			if (vehicleViewModel.Tires != null)
+			{
+				vehicle.Tires = new();
+				foreach (var tire in vehicleViewModel.Tires!)
+				{
+					vehicle.Tires.Add(new Tire() { Id = tire.TireId, TireStatusId = tire.TireStatusId, VehicleId = vehicle.Id });
+				}
+
+			}
 			using var db = new VehicleDatabaseContext();
-			AddOrUpdate(vehicle);
-			AddOrUpdate(vehicle);
-			db.SaveChanges();
+			vehicle = AddOrUpdate(vehicle);
+
+			return GetVehicleViewModel(vehicle.Id);
+
 		}
 
 
-		public void AddOrUpdate(VehicleDto vehicle)
+		public VehicleViewModel GetVehicleViewModel(int vehicleId)
 		{
 			using var db = new VehicleDatabaseContext();
-			if (db.Vehicles.Where(i => i.Id == vehicle.VehicleId).Count() > 0)
+			var vehicle = (from i in db.Vehicles
+						   where i.Id == vehicleId
+						   select i)
+							.Include(i => i.VehicleType)
+							.Include(i => i.Tires)
+							.ThenInclude(i => i.TireStatus).Single();
+
+			return ConvertToVehicleViewModel(vehicle);
+		}
+		public VehicleViewModel ConvertToVehicleViewModel(Vehicle vehicle)
+		{
+			VehicleViewModel vehicleDto = new VehicleViewModel();
+			vehicleDto.VehicleId = vehicle.Id;
+			vehicleDto.Vin = vehicle.VinNumber;
+			vehicleDto.Make = vehicle.Make;
+			vehicleDto.Model = vehicle.Model;
+			vehicleDto.Year = vehicle.Year;
+			vehicleDto.FuelLevel = vehicle.FuelLevel;
+			vehicleDto.VehicleTypeName = vehicle.VehicleType.Name;
+			vehicleDto.VehicleTypeId = vehicle.VehicleType.Id;
+			vehicleDto.Tires = vehicle.Tires.Select(i => new TireVm()
+			{
+				TireId = i.Id,
+				TireStatusId = i.TireStatus.Id,
+				TireStatus = i.TireStatus.Status
+			}).ToList();
+			return vehicleDto;
+		}
+
+		public Vehicle AddOrUpdate(Vehicle vehicle)
+		{
+			using var db = new VehicleDatabaseContext();
+			if (db.Vehicles.Where(i => i.Id == vehicle.Id).Count() > 0)
 			{
 				db.Update(vehicle);
 			}
 			else
 			{
+				vehicle.Id = default(int);
 				db.Add(vehicle);
+				db.SaveChanges();
 			}
 			if (vehicle.Tires != null)
 			{
 
 				foreach (var tire in vehicle.Tires)
 				{
-					if (db.Tires.Where(i => i.Id == tire.TireId).Count() > 0)
+					if (db.Tires.Where(i => i.Id == tire.Id).Count() > 0)
 					{
 						db.Update(tire);
 					}
@@ -86,9 +124,8 @@ namespace DavisMotorVehicles.Server.Controllers
 				}
 			}
 			db.SaveChanges();
+			return vehicle;
 		}
-
-
 
 	}
 }
