5c48c082b843915b695bd3dc7681a97b2bcd7073
AlexBaldwin42 <blackanode@gmail.com>
2023-01-29 21:36:01 -0700

    Added Test

--- DavisMotorVehicles/Tests/Controller/VehicleControllerTests.cs
+++ DavisMotorVehicles/Tests/Controller/VehicleControllerTests.cs
@@ -10,6 +10,9 @@ using DavisMotorVehicles.Server.Controllers;
 using FluentAssertions;
 using DavisMotorVehicles.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using NuGet.DependencyResolver;
+using BlazorStrap;
 
 namespace DavisMotorVehicles.Tests.Controller
 {
@@ -21,6 +24,81 @@ namespace DavisMotorVehicles.Tests.Controller
 			_vehicleRepository = A.Fake<IVehicleRepository>();
 		}
 
+
+		[Fact]
+		public void VehicleController_UpdateVehicle_ReturnNewVehicle()
+		{
+			//Arrange 
+			var vehicleViewModel = new VehicleViewModel()
+			{
+				VehicleId = default(int),
+				VehicleTypeName = "Car",
+				VehicleTypeId = 1,
+				FuelLevel = 50,
+				Model = "TestMode",
+				Make = "TestMake",
+				Vin = "TestVin",
+				Year = 2019,
+				Tires = new List<TireVm>(),
+			};
+			var returnedVehicleViewModel = new VehicleViewModel()
+			{
+				VehicleId = 1,
+				VehicleTypeName = "Car",
+				VehicleTypeId = 1,
+				FuelLevel = 50,
+				Model = "TestMode",
+				Make = "TestMake",
+				Vin = "TestVin",
+				Year = 2019,
+				Tires = new List<TireVm>(),
+			};
+			var vehicle = new Vehicle()
+			{
+				Id = default(int),
+				FuelLevel = vehicleViewModel.FuelLevel,
+				VinNumber = vehicleViewModel.Vin,
+				Make = vehicleViewModel.Make,
+				Model = vehicleViewModel.Model,
+				Year = vehicleViewModel.Year,
+				VehicleTypeId = vehicleViewModel.VehicleTypeId,
+				Tires = new List<Tire>(),
+				VehicleType = new VehicleType()
+				{
+					Id = vehicleViewModel.VehicleTypeId,
+					Name = vehicleViewModel.VehicleTypeName
+				}
+			};
+			var returnedVehicle = new Vehicle()
+			{
+				Id = returnedVehicleViewModel.VehicleId,
+				FuelLevel = vehicleViewModel.FuelLevel,
+				VinNumber = vehicleViewModel.Vin,
+				Make = vehicleViewModel.Make,
+				Model = vehicleViewModel.Model,
+				Year = vehicleViewModel.Year,
+				VehicleTypeId = vehicleViewModel.VehicleTypeId,
+				Tires = new List<Tire>(),
+				VehicleType = new VehicleType()
+				{
+					Id = vehicleViewModel.VehicleTypeId,
+					Name = vehicleViewModel.VehicleTypeName
+				}
+			};
+
+			A.CallTo(() => _vehicleRepository.UpdateVehicle(vehicle)).Returns(true);
+			A.CallTo(() => _vehicleRepository.GetVehicle(0)).Returns(returnedVehicle);
+			var controller = new VehicleController(_vehicleRepository);
+
+			//Act 			
+			var result = controller.UpdateVehicle(vehicleViewModel);
+
+			//Assert 			
+			result.Should().NotBeNull();
+			result.Should().BeOfType(typeof(OkObjectResult));
+			result.Should().BeEquivalentTo<VehicleViewModel>(vehicleViewModel, options=> options.ExcludingMissingMembers());
+		}
+
 		[Fact]
 		public void VehicleController_UpdateVehicle_ReturnOk()
 		{
@@ -28,18 +106,19 @@ namespace DavisMotorVehicles.Tests.Controller
 			var vehicles = A.Fake<ICollection<Vehicle>>();
 			var vehicle = A.Fake<Vehicle>();
 			var vehicleViewModel = A.Fake<VehicleViewModel>();
-
+			
 			A.CallTo(() => _vehicleRepository.GetVehicle(vehicleViewModel.VehicleId)).Returns(vehicle);
 			A.CallTo(() => _vehicleRepository.UpdateVehicle(vehicle));
 			var controller = new VehicleController(_vehicleRepository);
 
 			//Act 			
 			var result = controller.UpdateVehicle(vehicleViewModel);
-			
+
 			//Assert 			
 			result.Should().NotBeNull();
 			result.Should().BeOfType(typeof(OkObjectResult));
 		}
+
 		[Fact]
 		public void VehicleController_UpdateVehicle_ReturnBadRequest()
 		{
@@ -58,6 +137,7 @@ namespace DavisMotorVehicles.Tests.Controller
 			result.Should().NotBeNull();
 			result.Should().BeOfType(typeof(BadRequestResult));
 		}
+
 		[Fact]
 		public void VehicleController_GetVehicle_ReturnOk()
 		{
@@ -68,7 +148,7 @@ namespace DavisMotorVehicles.Tests.Controller
 
 			//Act 			
 			var result = controller.GetVehicles();
-			
+
 			//Assert 			
 			result.Should().NotBeNull();
 			result.Should().BeOfType(typeof(OkObjectResult));
